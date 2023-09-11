2e5a270956ecc75be4ca57a6560c7a312d427b06
Fabio Dorakiewicz Alves <104926689+PaquiBik@users.noreply.github.com>
2023-09-11 12:26:52 -0300

    h

--- Main.js
+++ Main.js
@@ -7,7 +7,7 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 let Contador = 0;
 while (Contador < 9) {
     Contador = Contador + 1
-ListaDeTeclas[Contador].onclick = ListaDeTeclas;
+ListaDeTeclas[Contador].onclick = tocamSomClap;
 
 console.log(Contador);
 }
