256d9ef9e0435e41790ceb1da69c7db54555eb99
Fabio Dorakiewicz Alves <104926689+PaquiBik@users.noreply.github.com>
2023-09-18 12:17:49 -0300

    u

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 //uma função
-function tocamSomClap(idElementoDoAudioNaoFuncional) {
+function tocarSom(idElementoDoAudioNaoFuncional) {
     // som que toca isolado
     document.querySelector(idElementoDoAudioNaoFuncional).play();
 }
@@ -13,7 +13,7 @@ while (Contador < ListaDeTeclas.length) {
     //contador mudando de variavel (ele + 1)
     //ListaDeTeclas[Contador].onclick = tocar('#som-tecla-clap');
     ListaDeTeclas[Contador].onclick = function () {
-        tocamSomClap('#som-tecla-clap');
+        tocarSom('#som-tecla-clap');
     }
     const jacareVermelho = ListaDeTeclas[Contador].classList[1];
 console.log(jacareVermelho);
