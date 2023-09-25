a049daf99caf935a383fd8f23a9f2c43d7374d5c
Fabio Dorakiewicz Alves <104926689+PaquiBik@users.noreply.github.com>
2023-09-25 11:31:23 -0300

    d

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 //uma função
 function tocamSomClap(idElementoDoAudioNaoFuncional) {
     // som que toca isolado
-    document.querySelector(idElementoDoAudioNaoFuncional).play();
+    document.querySelector(idElementoDoAudioNaoFuncional).play(IdSom);
 }
 
 //contante de uma lista
